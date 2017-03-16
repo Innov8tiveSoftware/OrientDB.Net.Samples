b3d7ad63ba945907f31aba3d2ee40d3e2d150324
Michael A. Davidson <michael.davidson@devinformatics.local>
2017-03-16 15:47:27 -0500

    Add project files.

---
+++ OrientDB.Net.Samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrientDB.Net.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
